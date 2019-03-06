a425e505566d9b4260d9b1a75f90a996dacbdec8
slgeng <gengsanlu@gmail.com>
2019-03-06 16:25:12 +0800

    首次提交

---
+++ test_main.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class TestMatch(unittest.TestCase):
+    def setUp(self):
+        print "set up ...."
+
+    def tearDown(self):
+        print "tear down ..."
+
+    def test_1(self):
+        print "test_1"
+        self.assertEqual(1, 1, "not success")
+
+    def test_2(self):
+        print "test_2"
+        self.assertIs(1, 2, "failed")
+
+
+if __name__ == '__main__':
+    unittest.main()
